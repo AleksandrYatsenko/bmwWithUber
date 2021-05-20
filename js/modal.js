a009ed239f5914e8df50cb0eeb75e7839d21f03c
Alex <yatsikxvii@gmail.com>
2021-05-20 23:50:59 +0300

    add support for all tabs

--- js/modal.js
+++ js/modal.js
@@ -1,4 +1,21 @@
-const moreElem = document.querySelector('.more');
+// const moreElem = document.querySelector('.more');
+// const modalElem = document.querySelector('.modal');
+// const openModal = () => {
+//   modalElem.classList.remove('hidden');
+// };
+// const closeModal = () => {
+//   modalElem.classList.add('hidden');
+// };
+// moreElem.addEventListener('click', openModal);
+// modalElem.addEventListener('click', (event) => {
+//   const target = event.target;
+//   if (target.classList.contains('overlay') || 
+//       target.classList.contains('modal__close')) {
+//         closeModal();
+//       }
+// });
+
+const moreElems = document.querySelectorAll('.more');
 const modalElem = document.querySelector('.modal');
 
 const openModal = () => {
@@ -9,7 +26,9 @@ const closeModal = () => {
   modalElem.classList.add('hidden');
 };
 
-moreElem.addEventListener('click', openModal);
+moreElems.forEach (moreElem => {
+  moreElem.addEventListener('click', openModal)
+});
 modalElem.addEventListener('click', (event) => {
   const target = event.target;
   if (target.classList.contains('overlay') || 
